91f652c9555641875b282f19d5b7f8d198738109
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-07-22 22:03:54 +0900

    Changing output format to mp4

--- demo.py
+++ demo.py
@@ -141,7 +141,7 @@ if __name__ == "__main__":
         if args.output:
             if os.path.isdir(args.output):
                 output_fname = os.path.join(args.output, basename)
-                output_fname = os.path.splitext(output_fname)[0] + ".mkv"
+                output_fname = os.path.splitext(output_fname)[0] + ".mp4"
             else:
                 output_fname = args.output
             assert not os.path.isfile(output_fname), output_fname
@@ -149,7 +149,7 @@ if __name__ == "__main__":
                 filename=output_fname,
                 # some installation of opencv may not support x264 (due to its license),
                 # you can try other format (e.g. MPEG)
-                fourcc=cv2.VideoWriter_fourcc(*"x264"),
+                fourcc=cv2.VideoWriter_fourcc(*"mp4v"),
                 fps=float(frames_per_second),
                 frameSize=(width, height),
                 isColor=True,
