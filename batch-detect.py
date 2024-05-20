3898dc5c735b370d027680ddea51d40d35a743e5
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-05-20 11:11:17 +0900

    Cleaning up things

--- batch-detect.py
+++ batch-detect.py
@@ -3,10 +3,8 @@ import sys
 sys.path.append(".")
 
 import argparse
+import json
 import multiprocessing as mp
-
-# import json
-import pickle
 from pathlib import Path
 
 import click
@@ -40,34 +38,37 @@ def setup_cfg(args):
     return cfg
 
 
-root = Path.cwd().parent
+root = Path.cwd()
 dataset = conf.active.dataset
 detector = conf.active.detector
 video_in_dir = root / conf[dataset].path
 generate_video = conf.unidet.detect.generate_videos
 video_out_dir = root / f"data/{dataset}/{detector}/detect/videos"
-# json_out_dir = root / f"data/{dataset}/{detector}/detect/json"
-out_dir = root / f"data/{dataset}/{detector}/detect/detection"
+json_out_dir = root / f"data/{dataset}/{detector}/detect/json"
 video_ext = conf[dataset].ext
 
+unidet_dir = root / "UniDet"
+unidet_config = unidet_dir / conf.unidet.detect.config
+unidet_checkpoint = unidet_dir / conf.unidet.detect.checkpoint
+
 assert_that(video_in_dir).is_directory().is_readable()
-assert_that(conf.unidet.detect.config).is_file().is_readable()
-assert_that(conf.unidet.detect.checkpoint).is_file().is_readable()
+assert_that(unidet_config).is_file().is_readable()
+assert_that(unidet_checkpoint).is_file().is_readable()
 
 mp.set_start_method("spawn", force=True)
 
 args = argparse.ArgumentParser()
-args.config_file = conf.unidet.detect.config
+args.config_file = unidet_config
 args.confidence_threshold = conf.unidet.detect.confidence
 args.parallel = conf.unidet.detect.parallel
-args.opts = ["MODEL.WEIGHTS", conf.unidet.detect.checkpoint]
+args.opts = ["MODEL.WEIGHTS", str(unidet_checkpoint)]
 
 setup_logger(name="fvcore")
 logger = setup_logger()
 logger.info("Arguments: " + str(args))
 
 print("Input:", video_in_dir)
-print("Output:", out_dir)
+print("Output:", json_out_dir)
 print("Generate video:", generate_video)
 print("Output video:", video_out_dir)
 
@@ -82,12 +83,14 @@ bar = tqdm(total=n_videos)
 for file in video_in_dir.glob(f"**/*{video_ext}"):
     action = file.parent.name
     video_in = cv2.VideoCapture(str(file))
+    n_frames = int(video_in.get(cv2.CAP_PROP_FRAME_COUNT))
     gen = demo.run_on_video(video_in)
-    # n_frames = int(video_in.get(cv2.CAP_PROP_FRAME_COUNT))
-    detection_data = []
+    detection_data = {}
     out_frames = []
 
     for i, (viz, pred) in enumerate(gen):
+        bar.set_description(f"{i}/{n_frames}")
+
         if generate_video:
             rgb = cv2.cvtColor(viz, cv2.COLOR_BGR2RGB)
             out_frames.append(rgb)
@@ -105,18 +108,13 @@ for file in video_in_dir.glob(f"**/*{video_ext}"):
             }
         )
 
-    # json_out_path = json_out_dir / action / file.with_suffix(".json").name
-    out_path = out_dir / action / file.with_suffix(".pkl").name
+    json_out_path = json_out_dir / action / file.with_suffix(".json").name
 
     video_in.release()
-    # json_out_path.parent.mkdir(parents=True, exist_ok=True)
-    out_path.parent.mkdir(parents=True, exist_ok=True)
+    json_out_path.parent.mkdir(parents=True, exist_ok=True)
 
-    # with open(json_out_path, "w") as json_file:
-    #     json.dump(detection_data, json_file)
-
-    with open(out_path, "w") as file:
-        pickle.dump(detection_data, file)
+    with open(json_out_path, "w") as json_file:
+        json.dump(detection_data, json_file)
 
     if generate_video:
         video_out_path = video_out_dir / action / file.with_suffix(".mp4").name
